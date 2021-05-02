16a74e133be60be1ff18545825ac7fec0acc5131
Shaktish Prajapati <pshaktish@gmail.com>
2021-05-02 21:05:05 +0530

    change in app.test.js file

--- src/App.js
+++ src/App.js
@@ -9,9 +9,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
-          Hello Shaktish!
-        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>Shaktish</p>
       </header>
     </div>
   );
